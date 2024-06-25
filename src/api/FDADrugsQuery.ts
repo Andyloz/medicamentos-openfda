9ef84d558f3c1acacee73c45c21ed5bfd7c278fa
Andyloz <lozandyloz@gmail.com>
2024-06-25 13:51:16 +0200

    docs of FDADrugsQuery

--- src/api/FDADrugsQuery.ts
+++ src/api/FDADrugsQuery.ts
@@ -3,12 +3,16 @@ import { FdaDrugEntry, FDAError } from './FDADrugs.ts'
 const DRUGS_URL = 'https://api.fda.gov/drug/drugsfda.json'
 const QUERY_LIMIT = 50
 
-/*
-* Used for ease a bit the building of queries
-* */
+/**
+ * Used for ease a bit the building of queries
+ * */
 export default class FDADrugsQuery {
   readonly params: URLSearchParams
 
+  /**
+   * Makes sure that the {@link https://open.fda.gov/apis/authentication/ authentication} and the
+   * {@link https://open.fda.gov/apis/query-parameters/ query limit} are set
+   * */
   private constructor() {
     this.params = new URLSearchParams()
     if (import.meta.env.VITE_API_KEY) {
@@ -17,6 +21,11 @@ export default class FDADrugsQuery {
     this.params.append('limit', `${QUERY_LIMIT}`)
   }
 
+  /**
+   * See {@link https://open.fda.gov/apis/query-syntax/ query syntax} and
+   * {@link https://www.accessdata.fda.gov/scripts/cder/daf/index.cfm?event=faq.page#howsearch explanation of how the
+   * search works}
+   * */
   static search(search: string) {
     const query = new FDADrugsQuery()
     const queryString =
