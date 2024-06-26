ae6f43ab58654a1d949007ef8b6bb57e4c3e1613
Andyloz <lozandyloz@gmail.com>
2024-06-26 23:08:54 +0200

    ts-ignored IndexError

--- src/pages/IndexError.tsx
+++ src/pages/IndexError.tsx
@@ -1,8 +1,8 @@
 import { useRouteError } from "react-router-dom";
 
 export default function IndexError() {
-  const error = useRouteError();
-  console.error(error);
+  const error = useRouteError()
+  console.error(error)
   // todo make it production ready
 
   return (
@@ -10,6 +10,7 @@ export default function IndexError() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
+        {/* @ts-ignore */}
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
